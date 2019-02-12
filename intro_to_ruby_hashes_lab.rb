869c2b88f726e0d10abf4137956ddf11997ac4ac
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-02-12 20:19:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly
 	       :two_pieces_owned => 50, 
 	       :three_pieces_owned => 100, 
 	       :four_pieces_owned => 200 
-	      }
+	      },
 	    :names => 
 	     {
 	        :reading_railroad => { 'mortgage_value' => "$100"}, 
